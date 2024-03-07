1fe31fbb0bdc308c75206b5262b010c40df75ae8
Ivan Gonzalez <hal308366@gmail.com>
2024-03-07 15:01:43 -0800

    Creación de componentes

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,11 @@
+import { TextInput, Button, Text, View } from "react-native";
+
+export default function LoginForm() {
+  return (
+    <View>
+      <TextInput placeholder="Email" />
+      <TextInput placeholder="Password" />
+      <Button title="Enviar" onPress={() => console.log("enviado")} />
+    </View>
+  );
+}
