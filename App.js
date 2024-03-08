a9de0815e918a889fc2446a762f8a087bc976e1b
Ivan Gonzalez <hal308366@gmail.com>
2024-03-08 14:54:15 -0800

    Agregado Drawer navigation y próximos pasos

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import "react-native-gesture-handler";
+import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import NavigationStack from "./src/navigation/NavigationStack";
+import "react-native-gesture-handler";
 import NavigationTab from "./src/navigation/NavigationTab";
+import NavigationDrawer from "./src/navigation/NavigationDrawer";
 
 export default function App() {
   return (
