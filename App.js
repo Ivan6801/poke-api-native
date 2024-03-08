bebcb86ea953a023923682f1a1c12d55d2d17220
Ivan Gonzalez <hal308366@gmail.com>
2024-03-08 14:27:35 -0800

    Agregado Tab navigation y SafeAreaView

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
-import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import NavigationStack from "./src/navigation/NavigationStack";
+import NavigationTab from "./src/navigation/NavigationTab";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <NavigationStack />
+      <NavigationTab />
     </NavigationContainer>
   );
 }
