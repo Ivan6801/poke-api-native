f965ec622a92ba14ad1e9f13f23d35d656ad286c
Ivan Gonzalez <hal308366@gmail.com>
2024-03-08 14:20:13 -0800

    Agregado stacks de navegacion

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginForm from './src/components/LoginForm';
+import "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import NavigationStack from "./src/navigation/NavigationStack";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoginForm />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <NavigationStack />
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
